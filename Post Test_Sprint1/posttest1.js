9c7ee0366afb2de3b8871e32b20872ff92ad6bfa
heeymala <67099321+heeymala@users.noreply.github.com>
2021-08-05 21:30:19 +0700

    Add files via upload

---
+++ Post Test_Sprint1/posttest1.js
@@ -0,0 +1,40 @@
+/* //SOAL A
+//NOMOR 1
+//MENAMBAHKAN BUKU
+var namaBuku = parseInt(prompt("Tambahkan nama buku: "));
+if (namaBuku === true){
+    document.write("Kamu menambahkan buku berjudul" + namaBuku + "<br>");
+} else {
+    document.write("Kamu tidak menambahkan buku apapun");
+} */
+
+
+//NOMOR 2
+/* var buku = [];
+buku [0] = 'Perahu Kertas';
+buku [1] = 'Aroma Karsa';
+buku [2] = 'Madilog';
+buku [3] = 'Embun Pagi';
+buku [4] = 'Dilan 1990'; */
+
+//NOMOR 3
+/* function pinjamBuku(buku1, buku2, buku3, buku4, buku5){
+    var totalBuku;
+    totalBuku = buku1 + buku2 + buku3 + buku4 + buku5 ;
+    return totalBuku;
+}
+
+document.write(buku1, buku3, buku5); */
+
+//NOMOR 4
+//NOMOR 5
+
+//SOAL B
+//NOMOR 1
+var elektronik = ["Telepon", "Mesin Cuci", "Kulkas", "Televisi", "Laptop"];
+
+//NOMOR 2
+
+
+
+
